3bc9366440040a3bd4ec5839a163196dca0724fb
araman0399-cell <araman0399@gmail.com>
2026-02-05 09:13:46 +0530

    Add files via upload

---
+++ project 4/script.js
@@ -0,0 +1,14 @@
+const slides = document.querySelectorAll('.slide');
+let currentSlide = 0;
+
+function showSlide(index) {
+    slides.forEach(slide => slide.classList.remove('active'));
+    slides[index].classList.add('active');
+}
+
+function nextSlide() {
+    currentSlide = (currentSlide + 1) % slides.length;
+    showSlide(currentSlide);
+}
+
+setInterval(nextSlide, 3000); // Change slide every 3 seconds
